69be57b9a06bb861a9eea7ecb089d7bc36ea57b4
selen çakıroğlu <selencakiroglu@gmail.com>
2019-04-06 00:54:29 +0300

    Added DBContext configurations.

--- Recipes.API/Startup.cs
+++ Recipes.API/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Recipes.API.Entities;
 
 namespace Recipes.API
 {
@@ -14,6 +16,9 @@ namespace Recipes.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=RecipesDB;Trusted_Connection=True;";
+            services.AddDbContext<RecipesContext>(o => o.UseSqlServer(connectionString));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
