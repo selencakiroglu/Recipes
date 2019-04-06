3a2df4d1aefb6aff8f949162f40a7bd6a14ded0c
selen çakıroğlu <selencakiroglu@gmail.com>
2019-04-06 20:10:46 +0300

    Completed entities for code first database initialization.

--- Recipes.API/Entities/RecipeCategory.cs
+++ Recipes.API/Entities/RecipeCategory.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Recipes.API.Entities
 {
     public class RecipeCategory
     {
+        [Key, Column(Order = 0)]
         public Guid RecipeId { get; set; }
+
         public Recipe Recipe { get; set; }
+
+        [Key, Column(Order = 1)]
         public Guid CategoryId { get; set; }
+
         public Category Category { get; set; }
     }
 }
