01258c6de41fa168e430a5328761153cb4bf0b67
selen çakıroğlu <selencakiroglu@gmail.com>
2019-04-06 20:12:17 +0300

    Added DummyController to test database initialization.

---
+++ Recipes.API/Controllers/DummyController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recipes.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipes.API.Controllers
+{
+    public class DummyController : ControllerBase
+    {
+        private RecipesContext _ctx;
+
+        public DummyController(RecipesContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        [Route("api/testdatabase")]
+        public IActionResult TestDatabase()
+        {
+            return Ok();
+        }
+    }
+}
