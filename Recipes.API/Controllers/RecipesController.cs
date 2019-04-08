ed901ebf480637f470fbaee140a4554dd0ca6141
selen çakıroğlu <selencakiroglu@gmail.com>
2019-04-09 00:30:30 +0300

    Completed AddRecipe endpoint.

--- Recipes.API/Controllers/RecipesController.cs
+++ Recipes.API/Controllers/RecipesController.cs
@@ -1,12 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Recipes.API.Entities;
 using Recipes.API.Helpers;
 using Recipes.API.Models;
 using Recipes.API.Services;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Recipes.API.Controllers
 {
@@ -20,7 +19,7 @@ namespace Recipes.API.Controllers
             _recipeRepository = recipeRepository;
         }
 
-        [HttpPost("all")]
+        [HttpPost("all", Name = "GetRecipes")]
         public IActionResult GetRecipes(
             [FromBody] RecipesResourceParameters recipesResourceParameters)
         {
@@ -35,6 +34,76 @@ namespace Recipes.API.Controllers
             }
 
             return Ok(results);
+        }
+
+        [HttpPut("add")]
+        public IActionResult CreateRecipe([FromBody] RecipeForCreationDto recipeDto)
+        {
+            if (recipeDto == null)
+            {
+                return StatusCode(400, "Wrong JSON object.");
+            }
+
+            if (recipeDto.Categories.Count() < 1)
+            {
+                ModelState.AddModelError(nameof(RecipeForCreationDto),
+                    "You should fill out at least one category.");
+            }
+
+            if (recipeDto.Ingredients.Count() < 1)
+            {
+                ModelState.AddModelError(nameof(RecipeForCreationDto),
+                    "You should fill out at least one ingredient.");
+            }
+
+            if (String.IsNullOrEmpty(recipeDto.Directions.Step))
+            {
+                ModelState.AddModelError(nameof(RecipeForCreationDto),
+                    "You should fill out direction step.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return new UnprocessableEntityObjectResult(ModelState);
+            }
+
+            if (_recipeRepository.RecipeExist(recipeDto.Title))
+            {
+                return StatusCode(409, "Recipe duplicated.");
+            }
+            
+            var recipeEntity = Mapper.Map<Recipe>(recipeDto);
+
+            foreach (var categoryName in recipeDto.Categories)
+            {
+                var recipeCategory = new RecipeCategory();
+                var category = _recipeRepository.GetCategory(categoryName);
+
+                if (category == null)
+                {
+                    recipeCategory.Category = new Category()
+                    {
+                        CategoryId = Guid.NewGuid(),
+                        Name = categoryName
+                    };
+                }
+                else
+                {
+                    recipeCategory.Category = category;
+                }
+                recipeEntity.RecipeCategories.Add(recipeCategory);
+            }
+
+            _recipeRepository.AddRecipe(recipeEntity);
+
+            if (!_recipeRepository.Save())
+            {
+                return StatusCode(500, "Internal error occured.");
+            }
+
+            var recipeToReturn = Mapper.Map<RecipeDto>(recipeEntity);
+
+            return CreatedAtRoute("GetRecipes", null, recipeToReturn);
         }
     }
 }
